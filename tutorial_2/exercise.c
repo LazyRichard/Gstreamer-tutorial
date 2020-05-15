16da29da0bbd74e4b28e4af4f0f2871a7c369b3d
Suh junmin <sof.midnight@live.co.kr>
2020-05-15 15:49:08 +0900

    add tutorial 2 graph

--- tutorial_2/exercise.c
+++ tutorial_2/exercise.c
@@ -7,6 +7,7 @@ int main(int argc, char *argv[])
     GstBus *bus;
     GstMessage *msg;
     GstStateChangeReturn ret;
+    gboolean terminate = FALSE;
 
     gst_init(&argc, &argv);
 
@@ -49,39 +50,69 @@ int main(int argc, char *argv[])
 
     /* Wait until error or EOS */
     bus = gst_element_get_bus(pipeline);
-    msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE, GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
 
-    /* Parse message */
-    if (msg != NULL)
+    do
     {
-        GError *err;
-        gchar *debug_info;
+        msg = gst_bus_timed_pop_filtered(
+            bus,
+            GST_CLOCK_TIME_NONE,
+            GST_MESSAGE_STATE_CHANGED | GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
 
-        switch (GST_MESSAGE_TYPE(msg))
+        if (msg != NULL)
         {
-        case GST_MESSAGE_ERROR:
-            gst_message_parse_error(msg, &err, &debug_info);
-            g_error("Error received from element %s: %s\n", GST_OBJECT_NAME(msg->src), err->message);
-            g_error("Debugging information: %s\n", debug_info ? debug_info : "none");
-            g_clear_error(&err);
-            g_free(debug_info);
+            GError *err;
+            gchar *debug_info;
 
-            break;
+            switch (GST_MESSAGE_TYPE(msg))
+            {
+            case GST_MESSAGE_ERROR:
+                gst_message_parse_error(msg, &err, &debug_info);
+                g_error("Error received from element %s: %s\n", GST_OBJECT_NAME(msg->src), err->message);
+                g_error("Debugging information: %s\n", debug_info ? debug_info : "none");
 
-        case GST_MESSAGE_EOS:
-            g_message("End-of-Stream reached.\n");
+                g_clear_error(&err);
+                g_free(debug_info);
 
-            break;
+                terminate = TRUE;
+                break;
+            case GST_MESSAGE_EOS:
+                g_message("End-Of-Stream reached.\n");
 
-        default:
-            /* We should not reach here because we only asked for ERRORs and EOS */
-            g_error("Unexpected message received.\n");
+                terminate = TRUE;
+                break;
 
-            break;
+            case GST_MESSAGE_STATE_CHANGED:
+                /* We are only interested in state-changed message from the pipeline */
+                if (GST_MESSAGE_SRC(msg) == GST_OBJECT(pipeline))
+                {
+                    GstState old_state, new_state, pending_state;
+
+                    gst_message_parse_state_changed(
+                        msg, &old_state, &new_state, &pending_state);
+                    g_message("Pipeline state change from %s to %s:\n",
+                              gst_element_state_get_name(old_state), gst_element_state_get_name(new_state));
+
+                    /* Create graph per state change */
+                    GString *base_fname = g_string_new("");
+                    g_string_printf(base_fname, "pipeline-%s-%s", gst_element_state_get_name(old_state), gst_element_state_get_name(new_state));
+
+                    GstBin *bin = GST_BIN(pipeline);
+                    GST_DEBUG_BIN_TO_DOT_FILE_WITH_TS(bin, GST_DEBUG_GRAPH_SHOW_MEDIA_TYPE, base_fname->str);
+
+                    g_string_free(base_fname, TRUE);
+                }
+
+                break;
+            default:
+                /* We should not reach here */
+                g_error("Unexpected message received.\n");
+
+                break;
+            }
+
+            gst_message_unref(msg);
         }
-
-        gst_message_unref(msg);
-    }
+    } while (!terminate);
 
     /* Free resources */
     gst_object_unref(bus);
